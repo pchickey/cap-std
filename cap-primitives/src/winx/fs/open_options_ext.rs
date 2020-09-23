540f13192f8abe35207404bd18533753fe40fc9f
Dan Gohman <dev@sunfishcode.online>
2020-09-22 19:15:41 -0700

    Add support for the new `open_options_ext_as_flags` on nightly.

--- cap-primitives/src/winx/fs/open_options_ext.rs
+++ cap-primitives/src/winx/fs/open_options_ext.rs
@@ -21,38 +21,36 @@ impl OpenOptionsExt {
             security_qos_flags: 0,
         }
     }
-}
 
-impl std::os::windows::fs::OpenOptionsExt for OpenOptionsExt {
-    fn access_mode(&mut self, mode: u32) -> &mut Self {
+    pub(crate) fn access_mode(&mut self, mode: u32) -> &mut Self {
         self.access_mode = Some(mode);
         self
     }
 
-    fn share_mode(&mut self, share: u32) -> &mut Self {
+    pub(crate) fn share_mode(&mut self, share: u32) -> &mut Self {
         self.share_mode = share;
         self
     }
 
-    fn custom_flags(&mut self, flags: u32) -> &mut Self {
+    pub(crate) fn custom_flags(&mut self, flags: u32) -> &mut Self {
         self.custom_flags = flags;
         self
     }
 
-    fn attributes(&mut self, attributes: u32) -> &mut Self {
+    pub(crate) fn attributes(&mut self, attributes: u32) -> &mut Self {
         self.attributes = attributes;
         self
     }
 
     /// Re-enable this once https://github.com/rust-lang/rust/pull/74074 is in stable.
     #[cfg(feature = "windows_security_qos_flags")]
-    fn security_qos_flags(&mut self, flags: u32) -> &mut Self {
+    pub(crate) fn security_qos_flags(&mut self, flags: u32) -> &mut Self {
         self.security_qos_flags = flags | winbase::SECURITY_SQOS_PRESENT;
         self
     }
 
     #[cfg(not(feature = "windows_security_qos_flags"))]
-    fn security_qos_flags(&mut self, _flags: u32) -> &mut std::fs::OpenOptions {
+    pub(crate) fn security_qos_flags(&mut self, _flags: u32) -> &mut std::fs::OpenOptions {
         panic!("OpenOptionsExt::security_qos_flags requires the \"nightly\" feature")
     }
 }
